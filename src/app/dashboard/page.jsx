9baa046b15bd1b9ea2db16e310a9e4fd8b5fda25
Sony Tom <mailsonytom@gmail.com>
2024-10-30 12:42:02 +0530

    Added Bar chart for statistics

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,4 +1,6 @@
+"use client";
 import OverViewCardPage from "../components/overviewcard";
+import BarChartStatistic from "../components/barChart";
 
 export default function Page() {
   return (
@@ -9,6 +11,9 @@ export default function Page() {
       <div className="graph-div flex">
         <div className="w-3/5">
           <h2>Graph</h2>
+          <div className="w-11/12 h-[400px] rounded">
+            <BarChartStatistic />
+          </div>
         </div>
         <div className="w-2/5">
           <h2>Recents table</h2>
